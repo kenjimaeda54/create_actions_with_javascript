8e7caa08fcbafa783f1ae332cf780b5dfd498446
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-01-27 22:38:04 -0300

    feat: create task issue

---
+++ .github/actions/isssue/index.js
@@ -0,0 +1,28 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+(async () => {
+  try {
+    const token = core.getInput("token");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
+    const title = core.getInput("title");
+    //https://octokit.github.io/rest.js/v18
+    const octokit = await new github.GitHub(token);
+    const { data } = await octokit.issues.create({
+      // owner: github.context.repo.owner,
+      // repo: github.context.repo.repo,
+      ...github.context.repo,
+      title,
+      body,
+      //assignees e um array,yml não aceita array
+      // entao o contrato vai ser usar | e aqui trato com split
+      assignees: assignees ? assignees.split("\n") : [],
+    });
+    //data nao vai ser um objeto por isso stringify
+    //primeiro parâmetro precisa ser idêntico declarado na saida do action
+    core.setOutput("issue", JSON.stringify(data));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
