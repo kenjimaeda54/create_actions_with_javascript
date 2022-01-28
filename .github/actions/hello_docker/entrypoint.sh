2dee20959c985e2d96bc6482c8e826559100a496
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-01-28 12:31:34 -0300

    feat: added image docker

---
+++ .github/actions/hello_docker/entrypoint.sh
@@ -0,0 +1,27 @@
+
+#https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions
+
+
+
+echo "::debug::Debug message"
+echo "::error::Error message"
+echo "::warning::Warning message"
+
+echo ::add-mask::$1
+echo "Hello  $1"
+
+echo "::group::Log group"
+echo group
+echo group
+echo group
+echo "::endgroup::"
+
+
+time=$(date)
+#time precisar ser o mesmo nome do arquivo action que esta sendo executado
+echo "::set-output name=time::$time"
+
+#https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
+# saida das variaveis, para o arquivo javascrpit
+# usou a lib core, dai utilizou core.exportVariable
+echo "action_state=Sendo sendo exportado pelo entrypoint" >> $GITHUB_ENV
